7d0a95774ef977752274b4e1745a7ca427766dc1
marinaPerezYnov <148948211+marinaPerezYnov@users.noreply.github.com>
2025-02-07 11:33:14 +0100

    - Configure database access - Add User entity to database

--- nest-e_commerce-app/apps/nest-e_commerce-app/src/app.module.ts
+++ nest-e_commerce-app/apps/nest-e_commerce-app/src/app.module.ts
@@ -3,24 +3,40 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './../../auth/src/auth.module';
 import { UsersModule } from 'apps/users/src/users.module';
-// import { TypeOrmModule } from '@nestjs/typeorm';
-// import { DataSource } from 'typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 import { ConfigModule } from '@nestjs/config';
-// import { User } from 'apps/users/src/users.entity';
+import { ConfigService } from '@nestjs/config';
+import { User } from 'apps/users/src/users.entity';
 
 @Module({
   imports: [
     UsersModule,
     AuthModule,
     ConfigModule.forRoot({
-      expandVariables: true,
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT_USER_MANAGER'),
+        username: configService.get<string>('DB_USERNAME_USER_MANAGER'),
+        password: configService.get<string>('DB_PASSWORD_USER_MANAGER'),
+        database: configService.get<string>('DB_NAME_USER_MANAGER'),
+        entities: [User],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [AppController],
   providers: [AppService],
 })
+
 // Le paramètre synchronize: true ne doit pas être utilisé en production, sinon vous risquez de perdre des données de production.
 export class AppModule {
   // le TypeORM DataSourceet EntityManagerles objets seront disponibles pour être injectés dans l'ensemble du projet
-  // constructor(private dataSource: DataSource) {}
+  constructor(private dataSource: DataSource) {}
 }
--- nest-e_commerce-app/apps/users/src/users.module.ts
+++ nest-e_commerce-app/apps/users/src/users.module.ts
@@ -1,20 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
-// import { TypeOrmModule } from '@nestjs/typeorm';
-// import { User } from './users.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users.entity';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [
-    //TypeOrmModule.forFeature([User]),
-    ConfigModule.forRoot({
-      expandVariables: true,
-    }),
-  ],
+  imports: [TypeOrmModule.forFeature([User]), ConfigModule],
   controllers: [UsersController],
   providers: [UsersService],
-  exports: [UsersService],
-  // exports: [UsersService, TypeOrmModule],
+  exports: [UsersService, TypeOrmModule],
 })
 export class UsersModule {}
