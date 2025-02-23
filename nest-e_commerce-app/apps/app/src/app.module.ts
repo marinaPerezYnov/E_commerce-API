6176f4c387853cbb9e02bcd8eb4a4ebb6c3017f4
marinaPerezYnov <marina.perez@ynov.com>
2025-02-23 15:00:22 +0100

    rename folder by app

--- nest-e_commerce-app/apps/app/src/app.module.ts
+++ nest-e_commerce-app/apps/app/src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './../../auth/src/auth.module';
+import { AuthModule } from '../../auth/src/auth.module';
 import { UsersModule } from 'apps/users/src/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DataSource } from 'typeorm';
