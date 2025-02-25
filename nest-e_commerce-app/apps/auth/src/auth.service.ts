dee782dfed4d938ada16ba39e22175f21e7f280c
marinaPerezYnov <148948211+marinaPerezYnov@users.noreply.github.com>
2025-02-25 16:37:50 +0100

    add token expired time

--- nest-e_commerce-app/apps/auth/src/auth.service.ts
+++ nest-e_commerce-app/apps/auth/src/auth.service.ts
@@ -46,7 +46,9 @@ export class AuthService {
 
     const payload = { sub: user.id, username: user.email };
     return {
-      access_token: await this.jwtService.signAsync(payload),
+      access_token: await this.jwtService.signAsync(payload, {
+        expiresIn: '1d',
+      }),
       user: user,
       error: '',
     };
